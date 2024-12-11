a3a70e9d5b3b12eb25c2cb2ddd0a5212d47796d6
RamsesMC113 <158788680+RamsesMC113@users.noreply.github.com>
2024-12-11 11:52:31 -0600

    Apollo cliente

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql', // URL de tu servidor GraphQL
+    uri: 'http://192.168.239.191:4000/graphql', // URL de tu servidor GraphQL
     cache: new InMemoryCache(),
 });
 
